cd7dbfdee70af1738c08629532fb6ffd76b4d425
Czosssnek <czosssnek@gmail.com>
2018-10-17 15:42:49 +0200

    TESDzzz

---
+++ Source/Test/MyBlueprint_AI_Behaviour.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "MyBlueprint_AI_Behaviour.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TEST_API UMyBlueprint_AI_Behaviour : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
